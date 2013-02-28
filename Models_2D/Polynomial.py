8e67a4f70a171b7f9bf90989bae92b29f47972e1
zunzun.com <zunzun.com@689c7514-71fa-63cc-3a28-60a07dff062d>
2013-02-28 12:26:32 +0000

    Intermediate work on custom polynomials

--- Models_2D/Polynomial.py
+++ Models_2D/Polynomial.py
@@ -17,7 +17,7 @@ import sys, os
 if os.path.join(sys.path[0][:sys.path[0].rfind(os.sep)], '..') not in sys.path:
     sys.path.append(os.path.join(sys.path[0][:sys.path[0].rfind(os.sep)], '..'))
 
-import pyeq2
+import pyeq2, pyeq2.PolyFunctions
 
 import numpy
 numpy.seterr(over = 'raise', divide = 'raise', invalid = 'raise', under = 'ignore') # numpy raises warnings, convert to exceptions to trap them
@@ -434,4 +434,106 @@ class UserSelectablePolynomial(pyeq2.Model_2D_BaseClass.Model_2D_BaseClass):
                 s += "\ttemp = " + coeffDesignators[i] + ";\n"
             else:
                 s += "\ttemp = temp * x_in + " + coeffDesignators[i] + ";\n"
+        return s
+
+
+
+class UserCustomizablePolynomial(pyeq2.Model_2D_BaseClass.Model_2D_BaseClass):
+    userSelectablePolyfunctionalFlag = True
+    _baseName = "User-Customizable Polynomial"
+    _leftSideHTML = 'y'
+    _canLinearSolverBeUsedForSSQABS = True
+    
+    webReferenceURL = ''
+
+    baseEquationHasGlobalMultiplierOrDivisor_UsedInExtendedVersions = False
+    autoGenerateOffsetForm = False
+    autoGenerateReciprocalForm = False
+    autoGenerateInverseForms = False
+    autoGenerateGrowthAndDecayForms = False
+
+    
+    def __init__(self, inFittingTarget = None, inExtendedVersionName = 'Default', inPolyfunctional2DFlags = [], inPolyfunctionalEquationList_X = []):
+        if not inPolyfunctionalEquationList_X:
+            self.polyfunctionalEquationList = pyeq2.PolyFunctions.GenerateListForCustomPolynomials_2D()
+        else:
+            self.polyfunctionalEquationList = inPolyfunctionalEquationList_X
+        
+        self.independentData1CannotContainZeroFlag = False
+        self.independentData1CannotContainPositiveFlag = False
+        self.independentData1CannotContainNegativeFlag = False
+        self.independentData2CannotContainZeroFlag = False
+        self.independentData2CannotContainPositiveFlag = False
+        self.independentData2CannotContainNegativeFlag = False
+    
+        pyeq2.Model_2D_BaseClass.Model_2D_BaseClass.__init__(self, inFittingTarget, inExtendedVersionName) # call superclass      
+    
+        self.polyfunctional2DFlags = inPolyfunctional2DFlags
+                
+        
+    def GetDisplayHTML(self):                
+        if not self.polyfunctional2DFlags:
+            self._HTML = "y = user-selectable function"
+        else:
+            self._HTML = 'y = </b>' # turn off any bolding
+            cd = self.GetCoefficientDesignators()
+            for index in range(len(self.polyfunctional2DFlags)):
+                self._HTML += '<b>' + cd[index] + '(</b> ' + self.polyfunctionalEquationList[self.polyfunctional2DFlags[index]].HTML + ' <b>)</b>'
+                if (self.polyfunctional2DFlags[index] != self.polyfunctional2DFlags[len(self.polyfunctional2DFlags)-1]) or (0 in self.polyfunctional2DFlags): # not the last one
+                    self._HTML += " + "
+            
+        return self.extendedVersionHandler.AssembleDisplayHTML(self)
+
+
+    def GetCoefficientDesignators(self):
+        self._coefficientDesignators = list(self.listOfAdditionalCoefficientDesignators[:len(self.polyfunctional2DFlags)])
+        return self._coefficientDesignators
+        
+    
+    def GetDataCacheFunctions(self):
+        functionList = []
+        for i in self.polyfunctional2DFlags:
+            if i == 0:
+                continue
+            functionList.append([pyeq2.DataCache.DataCacheFunctions.Polyfunctional2D(NameOrValueFlag=1, args=i), i])
+        if 0 in self.polyfunctional2DFlags:
+            functionList.append([pyeq2.DataCache.DataCacheFunctions.Polyfunctional2D(NameOrValueFlag=1, args=0), 0])
+        return functionList
+
+    
+    def ShouldDataBeRejected(self, unused):
+        for i in self.polyfunctional2DFlags:
+            self.independentData1CannotContainZeroFlag |= self.polyfunctionalEquationList[i].cannotAcceptDataWith_Zero
+            self.independentData1CannotContainPositiveFlag |= self.polyfunctionalEquationList[i].cannotAcceptDataWith_Positive
+            self.independentData1CannotContainNegativeFlag |= self.polyfunctionalEquationList[i].cannotAcceptDataWith_Negative
+        return pyeq2.Model_2D_BaseClass.Model_2D_BaseClass.ShouldDataBeRejected(self, unused)
+
+
+    def CalculateModelPredictions(self, inCoeffs, inDataCacheDictionary):
+        temp = 0.0
+        coeffCount = 0
+        try:
+            for i in self.polyfunctional2DFlags:
+                if i == 0:
+                    continue
+                temp += inCoeffs[coeffCount] * eval("inDataCacheDictionary['Polyfunctional2D_" + str(i) + "']")
+                coeffCount += 1
+            if 0 in self.polyfunctional2DFlags:
+                temp += inCoeffs[coeffCount] * eval("inDataCacheDictionary['Polyfunctional2D_0']")                
+            return self.extendedVersionHandler.GetAdditionalModelPredictions(temp, inCoeffs, inDataCacheDictionary, self)
+        except:
+            return numpy.ones(len(inDataCacheDictionary['DependentData'])) * 1.0E300
+
+
+    def SpecificCodeCPP(self):
+        s = ""
+        count = 0
+        EquationListForPolyfunctional = pyeq2.PolyFunctions.GenerateListForCustomPolynomials_2D()
+        for i in self.polyfunctional2DFlags:
+            if i != 0:
+                s += "\ttemp += " + self._coefficientDesignators[count] + " * " + EquationListForPolyfunctional[i].CPP + ";\n"
+                count += 1
+        if 0 in self.polyfunctional2DFlags:
+            s += "\ttemp += " + self._coefficientDesignators[count] + ";\n"
+                
         return s
