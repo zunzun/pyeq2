9955ad21c5f9518f366dbb85971e2a91e7d690e0
zunzun.com <zunzun.com@689c7514-71fa-63cc-3a28-60a07dff062d>
2013-07-08 17:07:48 +0000

    add javascript source code output

--- Services/OutputSourceCodeService.py
+++ Services/OutputSourceCodeService.py
@@ -281,6 +281,38 @@ class OutputSourceCodeService(object):
         return s
 
 
+    def GetOutputSourceCodeJAVASCRIPT(self, inEquation, inDigitsOfPrecisionString = '16'):
+        if inEquation.splineFlag == True:
+            if inEquation.GetDimensionality() == 2:
+                return self.SplineCodeJAVASCRIPT_2D(inEquation)
+            if inEquation.GetDimensionality() == 3:
+                return self.SplineCodeJAVASCRIPT_3D(inEquation)
+    
+        s = self.ConvertCppToJAVA(cppOutputSourceCodeUpperComment) # reuse existing code
+        s += '\n'
+        s += '// Fitting target: lowest ' + inEquation.fittingTargetDictionary[inEquation.fittingTarget] + '\n'
+        s += '// Fitting target value = ' + str(inEquation.CalculateAllDataFittingTarget(inEquation.solvedCoefficients)) + '\n\n'
+        if inEquation.GetDimensionality() == 2:
+            s += "function " + inEquation.extendedVersionHandler.AssembleSourceCodeName(inEquation) + "_model(x_in)\n\t{\n"
+        else:
+            s += "function " + inEquation.extendedVersionHandler.AssembleSourceCodeName(inEquation) + "_model(x_in, y_in)\n\t{\n"
+        s += '''\tvar temp;
+\ttemp = 0.0;
+
+\t// coefficients
+'''
+        cd = inEquation.extendedVersionHandler.AssembleCoefficientDesignators(inEquation)
+        tempString = " = %-." + inDigitsOfPrecisionString + "E"
+        for i in range(len(cd)):
+            s += "\tvar " + cd[i] + tempString % (inEquation.solvedCoefficients[i]) + ";\n"
+        s += "\n"
+        
+        s += self.ConvertCppToJAVA(inEquation.extendedVersionHandler.AssembleOutputSourceCodeCPP(inEquation)).replace('\t\ttemp', '\ttemp')
+
+        s += "\treturn temp;\n}\n"
+        return s
+
+
     def GetOutputSourceCodeSCILAB(self, inEquation, inDigitsOfPrecisionString = '16'):
         if inEquation.splineFlag == True:
             raise NotImplementedError('Not implemented for splines')
@@ -505,6 +537,159 @@ class OutputSourceCodeService(object):
         return s
 
 
+    def SplineCodeJAVASCRIPT_3D(self, inEquation):
+        s  = '''// To the best of my knowledge this code is correct.
+// If you find any errors or problems please contact
+// me at zunzun@zunzun.com.
+//      James
+//
+// the code below was partially based on the fortran code at:
+// http://svn.scipy.org/svn/scipy/trunk/scipy/interpolate/fitpack/fpbisp.f
+// http://svn.scipy.org/svn/scipy/trunk/scipy/interpolate/fitpack/fpbspl.f
+
+'''
+        s += "    function " + inEquation.__class__.__name__ + "_evaluation(x_in, y_in)\n"
+        s += '    {\n'
+        s += '        var tx = ['
+        for i in range(len(inEquation.scipySpline.get_knots()[0])):
+            s += "%-.16E" % (inEquation.scipySpline.get_knots()[0][i])
+            if i < (len(inEquation.scipySpline.get_knots()[0]) - 1):
+                s += ", "
+        s += '];\n'
+        s += '        var ty = ['
+        for i in range(len(inEquation.scipySpline.get_knots()[1])):
+            s += "%-.16E" % (inEquation.scipySpline.get_knots()[1][i])
+            if i < (len(inEquation.scipySpline.get_knots()[1]) - 1):
+                s += ", "
+        s += '];\n'
+        s += '        var coeff = ['
+        for i in range(len(inEquation.scipySpline.get_coeffs())):
+            s += "%-.16E" % (inEquation.scipySpline.get_coeffs()[i])
+            if i < (len(inEquation.scipySpline.get_coeffs()) - 1):
+                s += ", "
+        s += '];\n'
+        s += '        var nx = ' + str(len(inEquation.scipySpline.get_knots()[0])) + ';\n'
+        s += '        var ny = ' + str(len(inEquation.scipySpline.get_knots()[1])) + ';\n'
+        s += '        var kx = ' + str(inEquation.xOrder) + ';\n'
+        s += '        var ky = ' + str(inEquation.yOrder) + ';\n'
+        s += '''
+        var h = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        var hh = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        var w_x = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        var w_y = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
+        var i, j, li, lj, lx, ky1, nky1, ly, i1, j1, l2;
+        var f, temp;
+
+        var kx1 = kx+1;
+        var nkx1 = nx-kx1;
+        var l = kx1;
+        var l1 = l+1;
+
+        while ((x_in >= tx[l1-1]) && (l != nkx1))
+        {
+            l = l1;
+            l1 = l+1;
+        }
+        
+        h[0] = 1.0;
+        for (j = 1; j < kx+1; j++)
+        {
+            for (i = 0; i < j; i++)
+            {
+                hh[i] = h[i];
+            }
+            h[0] = 0.0;
+            for (i = 0; i < j; i++)
+            {
+                li = l+i;
+                lj = li-j;
+                if (tx[li] != tx[lj])
+                {
+                    f = hh[i] / (tx[li] - tx[lj]);
+                    h[i] = h[i] + f * (tx[li] - x_in);
+                    h[i+1] = f * (x_in - tx[lj]);
+                }
+                else
+                {
+                    h[i+1-1] = 0.0;
+                }
+            }
+        }
+        
+        lx = l-kx1;
+        for (j = 0; j < kx1; j++)
+        {
+            w_x[j] = h[j];
+        }
+
+        ky1 = ky+1;
+        nky1 = ny-ky1;
+        l = ky1;
+        l1 = l+1;
+
+        while ((y_in >= ty[l1-1]) && (l != nky1))
+        {
+            l = l1;
+            l1 = l+1;
+        }
+        
+        h[0] = 1.0;
+        for (j = 1; j < ky+1; j++)
+        {
+            for (i = 0; i < j; i++)
+            {
+                hh[i] = h[i];
+            }
+            h[0] = 0.0;
+            for (i = 0; i < j; i++)
+            {
+                li = l+i;
+                lj = li-j;
+                if (ty[li] != ty[lj])
+                {
+                    f = hh[i] / (ty[li] - ty[lj]);
+                    h[i] = h[i] + f * (ty[li] - y_in);
+                    h[i+1] = f * (y_in - ty[lj]);
+                }
+                else
+                {
+                    h[i+1-1] = 0.0;
+                }
+            }
+        }
+
+        ly = l-ky1;
+        for (j = 0; j < ky1; j++)
+        {
+            w_y[j] = h[j];
+        }
+
+        l = lx*nky1;
+        for (i1 = 0; i1 < kx1; i1++)
+        {
+            h[i1] = w_x[i1];
+        }
+            
+        l1 = l+ly;
+        temp = 0.0;
+        for (i1 = 0; i1 < kx1; i1++)
+        {
+            l2 = l1;
+            for (j1 = 0; j1 < ky1; j1++)
+            {
+                l2 = l2+1;
+                temp = temp + coeff[l2-1] * h[i1] * w_y[j1];
+            }
+            l1 = l1+nky1;
+        }
+            
+        return temp;
+    }
+'''
+        return s
+
+
     def SplineCodeJAVA_2D(self, inEquation):
         s  = '''// To the best of my knowledge this code is correct.
 // If you find any errors or problems please contact
@@ -593,6 +778,95 @@ class OutputSourceCodeService(object):
         return temp;
     }
 }
+'''
+        return s
+
+
+    def SplineCodeJAVASCRIPT_2D(self, inEquation):
+        s  = '''// To the best of my knowledge this code is correct.
+// If you find any errors or problems please contact
+// me at zunzun@zunzun.com.
+//      James
+//
+// the code below was partially based on the fortran code at:
+// http://svn.scipy.org/svn/scipy/trunk/scipy/interpolate/fitpack/splev.f
+// http://svn.scipy.org/svn/scipy/trunk/scipy/interpolate/fitpack/fpbspl.f
+
+'''
+        s += "    function " + inEquation.__class__.__name__ + "_evaluation(x_in)\n"
+        s += '    {\n'
+        s += '        var t = ['
+        for i in range(len(inEquation.scipySpline._eval_args[0])):
+            s += "%-.16E" % (inEquation.scipySpline._eval_args[0][i])
+            if i < (len(inEquation.scipySpline._eval_args[0]) - 1):
+                s += ", "
+        s += '];\n'
+        s += '        var coeff = ['
+        for i in range(len(inEquation.scipySpline._eval_args[1])):
+            s += "%-.16E" % (inEquation.scipySpline._eval_args[1][i])
+            if i < (len(inEquation.scipySpline._eval_args[1]) - 1):
+                s += ", "
+        s += '];\n'
+        s += '        var n = ' + str(len(inEquation.scipySpline._eval_args[0])) + ';\n'
+        s += '        var k = ' + str(inEquation.xOrder) + ';\n'
+        s += '''
+        var h = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        var hh = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
+        var i, j, li, lj, ll;
+        var f, temp;
+        
+        var k1 = k+1;
+        var l = k1;
+        var l1 = l+1;
+
+        while ((x_in < t[l-1]) && (l1 != (k1+1)))
+        {
+            l1 = l;
+            l = l-1;
+        }
+
+        while ((x_in >= t[l1-1]) && (l != (n-k1)))
+        {
+            l = l1;
+            l1 += 1;
+        }
+
+        h[0] = 1.0;
+        for (j = 1; j < k+1; j++)
+        {
+            for (i = 0; i < j; i++)
+            {
+                hh[i] = h[i];
+            }
+            h[0] = 0.0;
+            for (i = 0; i < j; i++)
+            {
+                li = l+i;
+                lj = li-j;
+                if (t[li] != t[lj])
+                {
+                    f = hh[i] / (t[li] - t[lj]);
+                    h[i] = h[i] + f * (t[li] - x_in);
+                    h[i+1] = f * (x_in - t[lj]);
+                }
+                else
+                {
+                    h[i+1] = 0.0;
+                }
+            }
+        }
+
+        temp = 0.0;
+        ll = l - k1;
+        for (j = 0; j < k1; j++)
+        {
+            ll = ll + 1;
+            temp = temp + coeff[ll-1] * h[j];
+        }
+        
+        return temp;
+    }
 '''
         return s
 
