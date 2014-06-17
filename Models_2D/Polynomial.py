ef3449c99617a44049c58281099c7df05c660105
zunzun.com <zunzun.com@689c7514-71fa-63cc-3a28-60a07dff062d>
2014-06-17 10:53:31 +0000

    Corrected obscure HTML typographical error

--- Models_2D/Polynomial.py
+++ Models_2D/Polynomial.py
@@ -475,7 +475,7 @@ class UserCustomizablePolynomial(pyeq2.Model_2D_BaseClass.Model_2D_BaseClass):
         if not self.polynomial2DFlags:
             self._HTML = "y = user-customizable polynomial"
         else:
-            self._HTML = 'y = </b>' # turn off any bolding
+            self._HTML = 'y = '
             cd = self.GetCoefficientDesignators()
             for index in range(len(self.polynomial2DFlags)):
                 self._HTML += '<b>' + cd[index] + '(</b> ' + self.polynomialEquationList[self.polynomial2DFlags[index]].HTML + ' <b>)</b>'
