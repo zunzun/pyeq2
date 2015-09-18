196f4128739ab372f594f3134922f247e3c0aab3
zunzun <zunzun@zunzun-laptop>
2015-09-18 11:28:03 -0500

    Work on new NodeJS examples

---
+++ Examples/NodeJS/LinearPolynomial_2D.js
@@ -0,0 +1,22 @@
+
+models_2D = require("./Models_2D.js");
+
+equationInfo = models_2D.Polynomial.Linear;
+
+console.log(equationInfo);
+
+var textData = "" +
+"  X        Y \n" +
+" 5.357    10.376 \n" +
+" 5.457    10.489 \n" +
+" 5.797    10.874 \n" +
+" 5.936    11.049 \n" +
+" 6.161    11.327 \n" +
+" 6.697    12.054 \n" +
+" 6.731    12.077 \n" +
+" 6.775    12.138 \n" +
+" 8.442    14.744 \n" +
+" 9.769    17.068 \n" +
+" 9.861    17.104 \n";
+
+console.log(textData);
---
+++ Examples/NodeJS/README.txt
@@ -0,0 +1,3 @@
+With the NodeJS package python-shell, NodeJS can
+make python calls into pyeq2 for curve fitting and
+surface fitting.
